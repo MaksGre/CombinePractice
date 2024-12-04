fc01c7ca074f90be8868e3fcc5cfa60dceac24a8
Максим Гребеножко <m.grebenozhko@tbank.ru>
2024-12-04 10:20:15 +0400

    Initial Commit

---
+++ CombinePractice/CombinePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombinePracticeApp.swift
+//  CombinePractice
+//
+//  Created by Maksim Grebenozhko on 04.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct CombinePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
