75f04358b6216ff87bbf8b76787eb4dd499acc1a
Максим Гребеножко <m.grebenozhko@tbank.ru>
2024-12-18 10:59:45 +0400

    Lesson 2 with refresh

--- CombinePractice/Lesson2/FirstCancellablePipelineView.swift
+++ CombinePractice/Lesson2/FirstCancellablePipelineView.swift
@@ -12,32 +12,71 @@ struct FirstCancellablePipelineView: View {
     @StateObject var viewModel = FirstCancellablePipelineViewModel()
     
     var body: some View {
-        HStack {
-            TextField("Ваше имя", text: $viewModel.name)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            Text(viewModel.validation)
+        VStack {
+            Spacer()
+            Text(viewModel.data)
+                .font(.title)
+                .foregroundStyle(.green)
+            
+            Text(viewModel.status)
+                .foregroundStyle(.blue)
+            
+            Spacer()
+            
+            Button {
+                viewModel.cancel()
+            } label: {
+                Text("Отменить подписку")
+                    .foregroundColor(.white)
+                    .padding(.horizontal)
+                    .padding(.vertical, 8)
+            }
+            .background(.red)
+            .cornerRadius(8)
+            .opacity(viewModel.status == "Запрос в банк..." ? 1.0 : 0.0)
+            
+            Button {
+                viewModel.refresh()
+            } label: {
+                Text("Запрос данных")
+                    .foregroundColor(.white)
+                    .padding(.horizontal)
+                    .padding(.vertical, 8)
+            }
+            .background(.blue)
+            .cornerRadius(8)
+            .padding()
         }
-        .padding()
-        
-        Button("Отмена подписки") {
-            viewModel.validation = ""
-            viewModel.anyCancellable?.cancel()
-        }
-        
     }
 }
 
 class FirstCancellablePipelineViewModel: ObservableObject {
-    @Published var name = ""
-    @Published var validation = ""
+    @Published var data = ""
+    @Published var status = ""
     
-    var anyCancellable: AnyCancellable?
+    var cancellable: AnyCancellable?
     
     init() {
-        anyCancellable = $name
-            .map { $0.isEmpty ? "❌" : "✅" }
-            .sink { value in
-                self.validation = value
+        cancellable = $data
+            .map { [unowned self] value -> String in
+                status = "Запрос в банк..."
+                return value
+                
             }
+            .delay(for: 5, scheduler: DispatchQueue.main)
+            .sink { [unowned self] value in
+                self.data = "Сумма всех счетов 1 млн."
+                self.status = "Данные получены"
+            }
+    }
+    
+    func refresh() {
+        data = "Перезапрос данных"
+    }
+    
+    func cancel() {
+        status = "Операция отменена"
+        cancellable?.cancel()
+        cancellable = nil
     }
 }
